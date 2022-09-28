a9884891bec57be040785dc9057f248f5583a3e4
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-09-28 23:43:03 +0900

    홈 화면 네비게이션 기능 추가

--- MuseumApp/App.jsx
+++ MuseumApp/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import type {Node} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+
 import {
   SafeAreaView,
   ScrollView,
@@ -8,6 +14,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Button
 } from 'react-native';
 
 import {
@@ -20,74 +27,88 @@ import {
 
 import JoinForm from './view/JoinForm';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+function WelcomeScreen({navigation}){
+    return (
+        <View style ={{flex : 1, alignItems : 'center', justifyContent : 'center', backgroundColor : 'red'}}>
+            <Text>어플 제목</Text>
+            <Button title="로그인" />
+            <Button title="회원가입" onPress={() => navigation.navigate('로동당 가입장부')} />
+        </View>
+    )
+}
+function JoinFormScreen({navigation}){
+//   const isDarkMode = useColorScheme() === 'dark';
+//
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+    return(
+        <View>
             <JoinForm />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+    )
+}
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+  return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="천리마" component={WelcomeScreen} />
+                <Stack.Screen name="로동당 가입장부" component={JoinFormScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
 });
 
 export default App;
