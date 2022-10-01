5ab41e07ff670e7592b4a3efd0ca91033ac1a7ee
CHANYEONG YUN <92909812+YUNCHANYEONG@users.noreply.github.com>
2022-10-01 11:33:58 +0900

    Revert "init"

--- MuseumApp/App.jsx
+++ MuseumApp/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import type {Node} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import JoinForm from './view/JoinForm';
-import PublicBtn from './util/PublicBtn';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,46 +25,90 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-
+import JoinForm from './view/JoinForm';
 
 function WelcomeScreen({navigation}){
     return (
-        <View style ={{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
-            <Text>Title</Text>
-            <View style={styles.btnBox}>
-                <PublicBtn title='Sign in' />
-                <PublicBtn title='Sign up' onPress={() => navigation.navigate('JoinForm')} />
-{/*                 <Button title="Sign up" /> */}
-            </View>
+        <View style ={{flex : 1, alignItems : 'center', justifyContent : 'center', backgroundColor : 'red'}}>
+            <Text>어플 제목</Text>
+            <Button title="로그인" />
+            <Button title="회원가입" onPress={() => navigation.navigate('로동당 가입장부')} />
         </View>
     )
 }
 function JoinFormScreen({navigation}){
+//   const isDarkMode = useColorScheme() === 'dark';
+//
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
     return(
         <View>
             <JoinForm />
         </View>
+
     )
 }
-
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
 const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
   return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Welcome" component={WelcomeScreen} />
-                <Stack.Screen name="JoinForm" component={JoinFormScreen} />
+                <Stack.Screen name="천리마" component={WelcomeScreen} />
+                <Stack.Screen name="로동당 가입장부" component={JoinFormScreen} />
             </Stack.Navigator>
         </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-    btnBox: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 10
-    }
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
 });
 
 export default App;
