e8b69f37c0d32de5cf1c88aafb41c42320bb5d46
LaSeunghyun <rmwl2356@gmail.com>
2022-10-03 20:08:41 +0900

    auth 커밋

--- MuseumApp/util/auth.jsx
+++ MuseumApp/util/auth.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { View, Text } from "react-native"
 /**
- * @key "키 명칭" 
- * @value "키 벨류"
+ * userId, Pw 검증과 비밀번호 일치 여부 관리
  */
 
 export default function Auth(userId, userPw, eqPassword){
+    const [isChecked, setIsChecked] = useState(false);
+
     let authId = userId;
     let authPw = userPw;
     let authEq = eqPassword;
@@ -24,14 +26,13 @@ export default function Auth(userId, userPw, eqPassword){
             <Text> 비밀번호가 일치하지 않습니다. </Text>
         </View>
     )
-    else return null
-    // const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
-    // if(!regExp.test(userId)) return false
-    // else return true
-
-    // // 숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력
-    // const regExp = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
-    // if(!regExp.test(userPw)) return false
-    // else return true
+    const regId = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i
+    if(!regId.test(userId)){} setIsChecked(false)
+    
+    // 숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력
+    const regPw = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+    if(!regPw.test(userPw)) setIsChecked(false)
+    
+    if(isChecked) return null;
     
 }
