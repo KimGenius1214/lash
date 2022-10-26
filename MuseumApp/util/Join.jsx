56baf130728058828b523b0543e261ef55f1c7df
LaSeunghyun <rmwl2356@gmail.com>
2022-10-26 13:01:49 +0900

    lash : idx값 uuid 생성

--- MuseumApp/util/Join.jsx
+++ MuseumApp/util/Join.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Join({navigation, authId, authPw, name, phone}){
 
@@ -7,11 +8,17 @@ export default function Join({navigation, authId, authPw, name, phone}){
     let userName = name;
     let userPhone = phone;
 
+    const uuid = () => {
+        const temp = uuidv4().split('-')
+        return temp[0] + temp[3] + temp[2] + temp[4] + temp[1];
+    }
+
     const result = async () =>
         await axios.post(`/v1/api/user/${userId}`,{
+            userIdx : uuid,
             userId : userId,
             userPw : userPw,
-            userName : userName,
+            name : userName,
             userPhone : userPhone
         }).catch(function(err) {
             return err
