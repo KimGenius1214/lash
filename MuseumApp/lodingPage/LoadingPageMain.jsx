040c15421393bf118d425bc111002349ec5ad28e
LaSeunghyun <rmwl2356@gmail.com>
2022-11-05 14:10:22 +0900

    lash:로딩페이지

---
+++ MuseumApp/lodingPage/LoadingPageMain.jsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function LoadingPageMain(){
+
+    return(
+        <View style={styled.container}>
+            <View style={styled.bodyContainer}>
+                <Image source={require('../image/inging.gif')} style={styled.imgStyle} />
+                <Text>The Museum Project</Text>
+            </View>
+        </View>
+    )
+}
+
+const styled = StyleSheet.create({
+    container : {
+        flex: 1,
+        alignSelf: 'center',
+    },
+    bodyContainer: {
+        height: '100%',
+        justifyContent: 'center'
+    },
+    imgStyle:{
+        width: 300,
+        height: 300
+    }
+})
--- MuseumApp/view/WelcomeScreen.jsx
+++ MuseumApp/view/WelcomeScreen.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import LoadingPageMain from '../lodingPage/LoadingPageMain';
 import PublicBtn from '../util/PublicBtn';
 
 export default function WelcomeScreen({navigation}){
+    const [loading, setLoading] = useState(false);
+
+    const loadPage = async() => {
+        setTimeout(() => {setLoading(true)}, 5000)
+    }
+
+    loadPage();
+
+    if(!loading) return <LoadingPageMain />
+    else {
     return (
-        <View style={styles.container}>
-            <View style={styles.titleWrapper}>
-                <Text style={styles.title}>제목이 들어갈 자리</Text>
+            <View style={styles.container}>
+                <View style={styles.titleWrapper}>
+                    <Text style={styles.title}>제목이 들어갈 자리</Text>
+                </View>
+                <View style={styles.btnBox}>
+                    <PublicBtn title="Sign in" onPress={() => navigation.navigate('LoginForm')}/>
+                    <PublicBtn title="Sign up" onPress={() => navigation.navigate('JoinForm')} />
+                </View>
             </View>
-            <View style={styles.btnBox}>
-                <PublicBtn title="Sign in" onPress={() => navigation.navigate('LoginForm')}/>
-                <PublicBtn title="Sign up" onPress={() => navigation.navigate('JoinForm')} />
-            </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
